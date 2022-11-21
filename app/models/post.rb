df15e2f020894609b1e263a20d1f0340f4d902ef
tomoaki_narukawa <tomo111911111@gmail.com>
2022-11-21 23:11:36 +0900

    postのビュー,useのビューCSS修正完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user, optional: true
   mount_uploader :picture, PictureUploader
+  validates :content, presence: true
 
   def user
     return User.find_by(id: self.user_id)
